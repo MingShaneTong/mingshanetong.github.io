ec2c737633092661b6c76677656facb0aa9a0be2
Ming Shane Tong <mstong800@gmail.com>
2023-11-15 19:58:10 +1300

    commit frontend

---
+++ frontend/src/app/post/page.tsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import ReactMarkdown from 'react-markdown';
+import { Box, Heading } from "@chakra-ui/layout";
+import rehypeRaw from 'rehype-raw'
+
+
+interface Response {
+  data: {
+    id: number;
+    attributes: {
+      Title: string;
+      Content: string;
+      createdAt: any;
+      updatedAt: any;
+      publishedAt: any;
+    };
+  }[];
+  meta: any;
+}
+
+export const getData = async () => {
+  const response = await axios.get("http://localhost:1337/api/posts", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const data: Response = response.data;
+  return data;
+};
+
+const IndexView = async () => {
+  let data = await getData();
+
+  return (
+    <>
+      <Box height="100vh" padding="10">
+        <Heading>My Blog</Heading>
+        <ReactMarkdown rehypePlugins={[rehypeRaw]}>{data.data[0].attributes.Content}</ReactMarkdown>
+      </Box>
+    </>
+  );
+};
+
+export default IndexView;
