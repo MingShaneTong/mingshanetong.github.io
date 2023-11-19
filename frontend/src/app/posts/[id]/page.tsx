c7b530ba09169246597a5602a90d90d74503d872
Ming Shane Tong <mstong800@gmail.com>
2023-11-19 21:29:26 +1300

    rendered post on view

--- frontend/src/app/posts/[id]/page.tsx
+++ frontend/src/app/posts/[id]/page.tsx
@@ -4,7 +4,7 @@ import { Box, Heading, Text } from "@chakra-ui/layout";
 import rehypeRaw from 'rehype-raw';
 import { apiUrl } from "@/config/api.ts";
 import qs from 'qs';
-import { Post } from "@/models/Post";
+import { Post, Section, Text as PostText } from "@/models/Post";
 
 interface Response {
   data: Post;
@@ -25,7 +25,6 @@ const getPostData = async (id: number) => {
     }
   );
 
-  console.log(`${apiUrl}/api/posts/${id}?${query}`);
   const response = await axios.get(`${apiUrl}/api/posts/${id}?${query}`, {
     headers: {
       Accept: "application/json",
@@ -37,16 +36,45 @@ const getPostData = async (id: number) => {
 
 const IndexView = async ({ params }: { params: { id: number } }) => {
   let response = await getPostData(params.id);
+  let sections = response.data.attributes.sections;
 
   return (
-    <>
-      <Box height="100vh" padding="10">
-        <Heading>{response.data.attributes.title}</Heading>
-        <Text>{JSON.stringify(response.data.attributes.sections)}</Text>
-        {/* <ReactMarkdown rehypePlugins={[rehypeRaw]}>{data}</ReactMarkdown> */}
-      </Box>
-    </>
+    <Box height="100vh" padding="10">
+      <Heading as='h1' size='xl'>{response.data.attributes.title}</Heading>
+      {sections.map(
+        (section, index) => 
+          <SectionView key={index} section={section}/>
+      )}
+    </Box>
   );
 };
+
+const SectionView = ({ section }: { section: Section }) => {
+  return (
+    <>
+      <Heading as='h2' size='lg'>{section.heading}</Heading>
+      {section.content.map(
+        (text, index) => 
+          <TextView key={index} text={text}/>
+      )}
+    </>
+  );
+}
+
+const TextView = ({ text }: { text: PostText }) => {
+  if (text.type == 'html') {
+    return (
+      <ReactMarkdown rehypePlugins={[rehypeRaw]}>
+        {text.content}
+      </ReactMarkdown>
+    );
+  } else {
+    return (
+      <ReactMarkdown>
+        {text.content}
+      </ReactMarkdown>
+    );
+  }
+}
 
 export default IndexView;
