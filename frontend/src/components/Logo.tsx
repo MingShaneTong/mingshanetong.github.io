3a11d9c707c65c143a0fa1e847ec4c35a25c1a58
Ming Shane Tong <mstong800@gmail.com>
2023-12-05 00:25:08 +1300

    add logo link

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -4,6 +4,7 @@ import Logo from "@/models/Logo"
 import api from "@/config/api";
 import styles from "./Logo.module.css";
 import { Manrope } from 'next/font/google';
+import Link from "next/link";
 
 const manrope = Manrope({ 
   weight: "500",
@@ -15,16 +16,18 @@ export default function LogoComponent({ logo, size = 36 }: { logo: Logo, size?: 
   var icon = computedColorScheme === "light" ? logo.attributes.light : logo.attributes.dark;
 
   return (
-    <Group gap="xs">
-      <Image 
-        src={api + icon.data.attributes.url}
-        width={size}
-        height={size} 
-        alt={icon.data.attributes.alternativeText}
-      /> 
-      <Text style={manrope.style} size="lg">
-        {logo.attributes.sitename}
-      </Text>
-    </Group>
+    <Link href="/">  
+      <Group gap="xs">
+        <Image 
+          src={api + icon.data.attributes.url}
+          width={size}
+          height={size} 
+          alt={icon.data.attributes.alternativeText}
+        /> 
+        <Text style={manrope.style} size="lg">
+          {logo.attributes.sitename}
+        </Text>
+      </Group>
+    </Link>
   );
 }
