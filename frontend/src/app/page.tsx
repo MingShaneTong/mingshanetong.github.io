eb809fda078f50989c5d17c9f12de2f8ea46a5a9
Ming Shane Tong <mstong800@gmail.com>
2023-12-31 01:04:53 +1300

    add key achievements

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,19 +4,19 @@ import { getHomePageData } from '@/controllers/pageController';
 import AboutMe from '@/components/Home/AboutMe';
 import Interests from '@/components/Home/Interests';
 import WorkExperience from '@/components/Home/WorkExperience';
+import KeyAchievements from '@/components/Home/KeyAchievements';
 
 export default async function Home() {
   let pageDataResponse = await getHomePageData();
   let pageData = pageDataResponse.data.attributes;
 
   return (
-    <Group>
+    <>
       <Hero data={pageData.hero}/>
       <AboutMe />
       <Interests />
-      {/* Skills */}
-      {/* Key Achievements */}
+      <KeyAchievements />
       <WorkExperience />
-    </Group>
+    </>
   )
 }
