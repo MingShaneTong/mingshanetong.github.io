d14069067a7f58ad53afd61a522bec7b83e4f595
Ming Shane Tong <mstong800@gmail.com>
2024-01-03 23:33:19 +1300

    update urls

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import { Group, Text, useComputedColorScheme } from "@mantine/core";
+import { Group, Text } from "@mantine/core";
 import Logo from "@/models/Logo"
-import { FRONTEND_API_URL } from "@/config/api";
+import { FRONTEND_UPLOAD_URL } from "@/config/api";
 import { Manrope } from 'next/font/google';
 import Link from "next/link";
 
@@ -11,28 +11,15 @@ const manrope = Manrope({
 });
 
 export default function LogoComponent({ logo, size = 36 }: { logo: Logo, size?: number }) {
-  // const computedColorScheme = useComputedColorScheme('light');
-  // var iconStyles = computedColorScheme === "light" ? 
-  //   [{ display: "block" }, { display: "none" }]: 
-  //   [{ display: "none" }, { display: "block" }];
-        
   return (
     <Link href="/">  
       <Group gap="xs">
         <Image 
-          src={FRONTEND_API_URL + logo.attributes.light.data.attributes.url}
+          src={FRONTEND_UPLOAD_URL + logo.attributes.light.data.attributes.url}
           width={size}
           height={size} 
           alt={logo.attributes.light.data.attributes.alternativeText}
-          // style={iconStyles[0]}
         />
-        {/* <Image 
-          src={FRONTEND_API_URL + logo.attributes.dark.data.attributes.url}
-          width={size}
-          height={size} 
-          alt={logo.attributes.dark.data.attributes.alternativeText}
-          style={iconStyles[1]}
-        /> */}
         <Text style={manrope.style} size="lg">
           {logo.attributes.sitename}
         </Text>
